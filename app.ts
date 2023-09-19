23cc0c913ec26c7b8f350b521aefd907abcc6e4f
seung.bok <seungbok.lee@huinnoaim.com>
2023-09-19 17:37:31 +0900

    add prettier + eslint

--- app.ts
+++ app.ts
@@ -1,26 +1,27 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
+
 dotenv.config()
 
 const app = express()
 const port = process.env.SERVER_PORT
 
 interface WelCome {
-    name: string
-    message: string
-    url: string
+  name: string
+  message: string
+  url: string
 }
 
 const data: Partial<WelCome> = {
-    name: 'boki',
-    message: 'welcome programmers',
-    url: 'https://code-boki.tistory.com/'
+  name: 'boki',
+  message: 'welcome programmers',
+  url: 'https://code-boki.tistory.com/',
 }
 
-app.get("/", (req: Request, res: Response) => {
-    res.send(data)
+app.get('/', (req: Request, res: Response) => {
+  res.send(data)
 })
 
 app.listen(port, () => {
-    console.log(`server is listening at localhost:${port}`);
+  console.log(`server is listening at localhost:${port}`)
 })
